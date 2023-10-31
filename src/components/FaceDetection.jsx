be4cd4dfe44c152d83d7f636c255784ea8d5399d
Rana Ahmed <ranaete14@gmail.com>
2023-10-31 23:35:56 +0600

    face-detection added

---
+++ src/components/FaceDetection.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FaceDetection = () => {
+  return (
+    <View>
+      <Text>FaceDetection</Text>
+    </View>
+  )
+}
+
+export default FaceDetection
