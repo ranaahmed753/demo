567871280efb0662ee2de038f4f259c42a5ee585
Rana Ahmed <ranaete14@gmail.com>
2023-11-01 00:03:29 +0600

    nrvrhrhvhr

---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserProfile = () => {
+  return (
+    <View>
+      <Text>UserProfile This is bad thing</Text>
+      <Text>UserProfile This is bad thing</Text>
+      <Text>UserProfile This is bad thing</Text>
+      <Text>UserProfile This is bad thing</Text>
+      <Text>UserProfile This is bad thing</Text>
+      <Text>UserProfile This is bad thing</Text>
+      <Text>UserProfile This is bad thing</Text>
+    </View>
+  )
+}
+
+export default UserProfile
